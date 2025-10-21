5e741c0c7f2300c791903ebc6caccd26af68f2e5
Charlie Holtz <charlieholtz@gmail.com>
2025-10-20 19:55:45 -0700

    Add Homebrew cask for Conductor
    Initial release of the Conductor Homebrew tap.
    
    Users can now install Conductor with:
    brew install meltylabs/conductor/conductor

---
+++ Casks/conductor.rb
@@ -0,0 +1,28 @@
+cask "conductor" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.15.3"
+  sha256 :no_check
+
+  url "https://cdn.crabnebula.app/download/melty/conductor/latest/Conductor_#{version}_#{arch}.dmg",
+      verified: "cdn.crabnebula.app/download/melty/conductor/"
+  name "Conductor"
+  desc "Mac app that lets you run coding agents in parallel"
+  homepage "https://conductor.build/"
+
+  livecheck do
+    url "https://cdn.crabnebula.app/update/melty/conductor/darwin-#{arch}/0.0.0"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  app "Conductor.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.conductor.app",
+    "~/Library/Caches/com.conductor.app",
+    "~/Library/Preferences/com.conductor.app.plist",
+    "~/Library/Saved Application State/com.conductor.app.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,35 @@
+# Homebrew Tap for Conductor
+
+This is the official Homebrew tap for [Conductor](https://conductor.build/), a Mac app that lets you run coding agents in parallel.
+
+## Installation
+
+```bash
+brew install meltylabs/conductor/conductor
+```
+
+Or install via URL:
+
+```bash
+brew tap meltylabs/conductor
+brew install conductor
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade conductor
+```
+
+## Uninstalling
+
+```bash
+brew uninstall conductor
+```
+
+To remove all associated data:
+
+```bash
+brew uninstall --zap conductor
+```
